6f052c7fcd32c5180882bb8d56fe06d53ab428b8
Jean-Yves Migeon <jeanyves.migeon@free.fr>
2016-08-09 18:14:52 +0200

    The `D' can also return a '.' as root entry.

--- libarchive/archive_read_support_format_mtree.c
+++ libarchive/archive_read_support_format_mtree.c
@@ -560,7 +560,7 @@ bid_entry(const char *p, ssize_t len, ssize_t nl, int *last_is_path)
 	if (f == 0) {
 		const char *pb = p + len - nl;
 		int name_len = 0;
-		int slash;
+		int valid;
 
 		/* The form D accepts only a single line for an entry. */
 		if (pb-2 >= p &&
@@ -569,17 +569,23 @@ bid_entry(const char *p, ssize_t len, ssize_t nl, int *last_is_path)
 		if (pb-1 >= p && pb[-1] == '\\')
 			return (-1);
 
-		slash = 0;
+		valid = 0;
 		while (p <= --pb && *pb != ' ' && *pb != '\t') {
 			if (!safe_char[*(const unsigned char *)pb])
 				return (-1);
 			name_len++;
-			/* The pathname should have a slash in this
-			 * format. */
+			/* The pathname should either be '.' (root) or
+			 * have a slash in this format. */
 			if (*pb == '/')
-				slash = 1;
+				valid = 1;
+			if (*pb == '.') {
+				if (nl == 1 && pb[1] == '\n')
+					valid = 1;
+				if (nl == 2 && pb[1] == '\r' && pb[2] == '\n')
+					valid = 1;
+			}
 		}
-		if (name_len == 0 || slash == 0)
+		if (name_len == 0 || valid == 0)
 			return (-1);
 		/* If '/' is placed at the first in this field, this is not
 		 * a valid filename. */
